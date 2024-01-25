9a22a3661939f1600f483dc08d1e42d85839ea27
prasanth-developer <148607602+prasanth-developer@users.noreply.github.com>
2024-01-25 17:13:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+function getValue() {
+
+    let name = document.getElementById('name').value;
+    let chapter = document.getElementById('chapter').value;
+    let verse = document.getElementById('verse').value;
+
+    fetch(`https://bible-api.com/${name}${chapter}:${verse}`)
+        .then((data) => data.json())
+        .then((res) => {
+            console.log(res);
+            console.log(res.reference);
+            console.log(res.verses[0].text);
+
+            document.getElementById('bookname').innerHTML = `Book Name <br><span id="o-color" class="fs-1 text-capitalize">${res.verses[0].book_name}<span>`;
+            document.getElementById('bookofchapter').innerHTML = `Chapter <br><span id="o-color" class="fs-1 text-capitalize"> ${res.verses[0].chapter}<span>`;
+            document.getElementById('numofverse').innerHTML = `Verse <br><span id="o-color" class="fs-1 text-capitalize">${res.verses[0].verse}<span>`;
+            document.getElementById('text').innerHTML = `Content <br><span id="o-color" class="fs-3 text-capitalize">${res.verses[0].text}<span>`;
+            document.getElementById('reference').innerHTML = `Reference<br><span id="o-color" class="fs-1 text-capitalize">${res.reference}<span>`;
+            document.getElementById('translationname').innerHTML = `Translation Name<br><span id="o-color" class="fs-3 text-capitalize">${res.translation_name}<span>`;
+
+        })
+};
